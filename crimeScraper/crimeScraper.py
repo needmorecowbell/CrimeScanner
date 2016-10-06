f301004244da278f6fc1315f27e5a5c796af03ae
Adam Musciano <amusciano@gmail.com>
2016-10-06 16:09:30 +0000

    Renamed to index.html and added backend scraper functionality

--- crimeScraper/crimeScraper.py
+++ crimeScraper/crimeScraper.py
@@ -1,55 +1,63 @@
 
-import urllib.request
+import requests
+import csv
 from bs4 import BeautifulSoup
 
 
-
-for x in range(0,14):
-    print(x)
-    with urllib.request.urlopen("http://police.psu.edu/daily-crime-log?field_reported_value[value]&page="+str(x)) as url:
-        s = url.read()
-
-
-    tree = BeautifulSoup(s,"html.parser")
-    good_html = tree.getText()
-    good_html=good_html[good_html.find("Incident #:"):good_html.find("Charges/Citations:")]
-
-
-    incidentArr=good_html.splitlines()
-
-
-
-    # myfile= open("log.txt","a")
-    # for x in range(0, incidentArr.__len__()):
-    #     line= incidentArr[x]
-    #     if(line.find("Incident #")!=-1):
-    #         id=line[line.find(":")+3:line.__len__()]
-    #         myfile.write(id)
-    #         myfile.write(";")
-    #     elif(line.find("Reported:")!=-1):
-    #         timeReported = line[line.find(":") + 3:line.__len__()]
-    #         myfile.write(timeReported)
-    #         myfile.write(";")
-    #     elif(line.find("Nature of the Incident:")!=-1):
-    #         incidentNature = line[line.find(":") + 3:line.__len__()]
-    #         myfile.write(incidentNature)
-    #         myfile.write(";")
-    #     elif(line.find("Offenses")!=-1):
-    #         offenses = line[line.find(":") + 3:line.__len__()]
-    #         myfile.write(offenses)
-    #         myfile.write(";")
-    #     elif(line.find("Location")!=-1):
-    #         location = line[line.find(":") + 3:line.__len__()]
-    #         myfile.write(location)
-    #         myfile.write(";")
-    #     elif(line.find("Disposition")!=-1):
-    #         disposition = line[line.find(":") + 3:line.__len__()]
-    #         Values= [id,timeReported,incidentNature,offenses,location,disposition]
-    #         myfile.write(disposition)
-    #         myfile.write("\n")
+def logDataCSV(separated):
+    myfile = open("log.txt", 'a')
+    for element in separated:
+        #print(element+" TEST")
+        myfile.write(element+";")
+    myfile.write("\n")
+   
+    
+    
+def separateResults(html):
+    """Displays text of separated div tags in form of a
+    dictionary ("incident#","occurred","reported","nature","offenses","location","disposition")"""
+    #ans=["incident#","occurred","reported","nature","offenses","location","disposition"] 
+    
+    elements=html.text.splitlines() #split information by each new line into list
+    ans=[]#start empty list
+    
+    for element in elements:
+        if(len(element)>1):
+            if(element.find("charged with")):
+                ans.append(element[element.find(":")+1:])
+            else:
+                ans.append(element[element.find(":")+3:])#substring so only information is shown, then append
+        
+    return ans
 
 
 
 
+pageNum=0
+dataAvailable=True
 
-#http://www.police.psu.edu/psu-police/daily-crime-log.cfm
+while dataAvailable: # are there still pages to scrape
+
+    print("Page: "+str(pageNum))
+    url= "http://police.psu.edu/daily-crime-log?field_reported_value[value]&page="+str(pageNum) # url+ page incrementation
+    raw= requests.get(url)# request the raw html text from psu incident log
+    
+    soup = BeautifulSoup(raw.text,"html.parser")# lets take this raw text and put it into a filter that does nothing yet.
+
+    crimeSet=soup.find_all("div",{"class": "views-row"})# gets each chunk of crime log (holds all data for each incident)
+
+    #print(crimeSet)  
+    if(len(crimeSet)>0): #is the crimeSet empty
+    
+    
+        for crime in crimeSet:#iterate through every crime
+           separated= separateResults(crime)#store clean data in list
+           #log data from list
+           logDataCSV(separated)
+    else:
+        dataAvailable=False #no more data
+        
+        
+    pageNum+=1
+    
+
